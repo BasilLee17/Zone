a5fb0c777e2e35937964b3bb96b9e52184bb62a1
israelrmartinez <israel.r.mar@gmail.com>
2022-05-31 21:48:44 -0700

    calendar view is working

---
+++ zoneApp/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  zoneApp
+//
+//  Created by stlp on 5/31/22.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}
