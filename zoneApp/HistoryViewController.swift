83910ce931943cfc17786c8881cc55d8cbb8cd3a
israelrmartinez <israel.r.mar@gmail.com>
2022-06-05 23:31:23 -0700

    day numbers now are only blue when theres data

--- zoneApp/HistoryViewController.swift
+++ zoneApp/HistoryViewController.swift
@@ -11,9 +11,9 @@ var selectedDate = Date()
 var selectedDay = Date()
 var dayReview = ""
 var moodHistory = [
-    ["date": "June 6, 2022", "mood": "1", "dayOfMonth": "6", "month": "June"],
-    ["date": "June 12, 2022", "mood": "5", "dayOfMonth": "12", "month": "June"],
-    ["date": "June 27, 2022", "mood": "5", "dayOfMonth": "27", "month": "June"]
+    ["date": "June 6, 2022", "mood": "1", "dayOfMonth": "6", "month": "June 2022"],
+    ["date": "June 12, 2022", "mood": "5", "dayOfMonth": "12", "month": "June 2022"],
+    ["date": "June 27, 2022", "mood": "5", "dayOfMonth": "27", "month": "June 2022"]
 ]
 var dateEntry: [String:String] = [:]
 
@@ -99,9 +99,12 @@ class HistoryViewController: UIViewController, UICollectionViewDelegate, UIColle
         cell.moodEmoji.text = "🫥"
         cell.dayOfMonth.setTitleColor(UIColor.red, for: .normal)
         if totalSquares[indexPath.item] != "" {
+//            let currentMonth = monthLabel.text
             for mood in moodHistory {
 //                let squareMood: Int? = Int(mood["dayOfMonth"] ?? 0)
-                if mood["dayOfMonth"]! == totalSquares[indexPath.item] {
+//                print("mood month: \(mood["month"])")
+//                print("month label: \(String(describing: monthLabel.text))")
+                if mood["dayOfMonth"]! == totalSquares[indexPath.item] && mood["month"] == monthLabel.text {
                     print("day: \(String(describing: mood["dayOfMonth"]!))")
                     print("squares: \(totalSquares[indexPath.item])")
                     switch mood["mood"] {
